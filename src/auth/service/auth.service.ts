771d93b91a344f4e50a7bf98b6ea4f75137318e7
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-02-04 13:04:14 +0530

    # error fixed

--- src/auth/service/auth.service.ts
+++ src/auth/service/auth.service.ts
@@ -3,23 +3,24 @@ import { JwtService } from '@nestjs/jwt';
 import { from } from 'rxjs';
 import { Observable } from 'rxjs';
 import { User } from 'src/user/models/user.interface';
-import { bcrypt } from 'bcrypt';
+//import { bcrypt } from 'bcrypt';
+const bcrypt = require('bcrypt');
 import { of } from 'rxjs';
 
 @Injectable()
 export class AuthService {
 
-    constructor(private readonly _jwtService: JwtService){}
+  constructor(private readonly _jwtService: JwtService) { }
 
-    generateJwt(user: User): Observable<string>{
-      return from(this._jwtService.sign({user}));
-    }
+  generateJwt(user: User): Observable<string> {
+    return from(this._jwtService.signAsync({ user }));
+  }
 
-    hashPassword(password: string): Observable<string>{
-      return from<string>(bcrypt.hash(password, 12));
-    }
+  hashPassword(password: string): Observable<string> {
+    return from<string>(bcrypt.hash(password, 12));
+  }
 
-    comparePasswords(newPassword: string, passwordHash: string): Observable<any | boolean>{
-      return of<any | boolean>(bcrypt.compare(newPassword, passwordHash));
-    }
+  comparePasswords(newPassword: string, passwordHash: string): Observable<any | boolean> {
+    return of<any | boolean>(bcrypt.compare(newPassword, passwordHash));
+  }
 }
--- src/user/controller/user.controller.ts
+++ src/user/controller/user.controller.ts
@@ -10,22 +10,22 @@ export class UserController {
     constructor(private _userService: UserService) { }
 
     @Post()
-    create(@Body() user: User): Observable<User| Object> {
+    create(@Body() user: User): Observable<User | Object> {
         //return this._userService.create(user);
         return this._userService.create(user).pipe(
             map((user: User) => user),
-            catchError(err => of({error: err.message}))
+            catchError(err => of({ error: err.message }))
         )
     }
 
 
     @Post('login')
-    login(@Body()user: User): Observable<Object>{
-       return this._userService.login(user).pipe(
-           map((jwt: string) =>{
-               return { access_token: jwt };
-           });
-       )
+    login(@Body() user: User): Observable<Object> {
+        return this._userService.login(user).pipe(
+            map((jwt: string) => {
+                return { access_token: jwt };
+            })
+        )
     }
 
 
