11a6185676c549ff551ad5730d7806982abc7735
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-02-22 13:54:26 +0530

    # adding login method in service and login component.ts

--- front-end/src/app/components/login/login.component.ts
+++ front-end/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../../services/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthenticationService) { }
 
   ngOnInit(): void {
+  }
+
+  login() {
+    return this.authService.login('******@gamil.com', '*******').subscribe(data => console.log('SUCCESSFUL LOGIN'));
   }
 
 }
