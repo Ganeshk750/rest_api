8f6053c97a844a84020514ab7fcee8124d29bc36
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-03-04 12:58:18 +0530

    adding validation in form using custome validators

--- front-end/src/app/components/register/register.component.ts
+++ front-end/src/app/components/register/register.component.ts
@@ -1,8 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../../services/authentication.service';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { map } from 'rxjs/operators';
+
+
+class CustomeValidators {
+  static passwordContainsNumber(controle: AbstractControl): ValidationErrors {
+    const regax = /\d/;
+    if(regax.test(controle.value) && controle.value !== null){
+      return null;
+    }else{
+      return {passwordInvalid: true};
+    }
+  }
+  static passwordMatches(control: AbstractControl): ValidationErrors {
+    const password = control.get('password').value;
+    const passwordConfirms = control.get('passwordConfirm').value;
+    if((password === passwordConfirms) && (password !== null && passwordConfirms !== null)){
+        return null;
+    }else{
+      return { passwordNotMatching: true};
+    }
+  }
+}
 
 @Component({
   selector: 'app-register',
@@ -27,11 +48,11 @@ export class RegisterComponent implements OnInit {
         Validators.minLength(6)
       ]],
       password:[null, [Validators.required, Validators.minLength(3),
-       // CustomeValidators.passwordContainsNumber;
+       CustomeValidators.passwordContainsNumber
       ]],
       passwordConfirm: [null, [Validators.required]]
     },{
-       // validators: CustomValidators.passwordMatches
+        validators: CustomeValidators.passwordMatches
     })
   }
 
